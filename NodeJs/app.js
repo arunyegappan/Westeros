e0ff2f9ae5099060315a05d7370c6117c8fbc1f4
Y.Arun Ramanathan <y.arunram@gmail.com>
2017-11-08 00:47:03 -0800

    Game initate logic with 2 players.
    Setup database, UI board with 2 players, player details, game start
    message handlers

--- NodeJs/app.js
+++ NodeJs/app.js
@@ -1,10 +1,15 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/westeros";
+var gameId;
+var dbResponse;
 
 //Handle from post data
 var bodyParser = require('body-parser');
 var connections = [];
+var numberOfConnections = 2;
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 
@@ -24,14 +29,30 @@ server.listen(8080, function() {
 io.sockets.on('connection', function(socket) {
   
   connections.push(socket.id); 
-  console.log(connections);
+  //console.log(connections);
   
-  socket.emit('gameStart',{playerId:connections.length});
+  //wait until u have min required players to start a game
+  if(connections.length == numberOfConnections)
+  {
+    //game can start. We Have required number of players
+    //create new document in db for this game
+    dbResponse = getInitialDb();
+    gameId = createNewDocumentInDb(dbResponse);
+  }
+  else
+  {
+    //tell the connected client to wait until other players join
+    console.log('waiting for more players to join');
+    socket.emit("wait");
+  }
 
+  
+
+  //socket request handlers
 
   socket.on('rollDice', function (data) {
       console.log("playerId" +data.playerId);
-      var diceNumber = randomIntFromInterval(2,12);
+      var diceNumber = randomIntFromInterval(2,6);
       console.log("rollDice: "+diceNumber)
       io.sockets.emit('move', { playerId:data.playerId,diceNumber:diceNumber });
     });
@@ -59,4 +80,74 @@ function removePlayer(item)
 {
 var index = connections.indexOf(item);
 connections.splice(index, 1);
+}
+
+
+function createNewDocumentInDb(dbResponse){
+  var gameId;
+  MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      db.collection("game").insertOne(dbResponse, function(err, res) {
+        if (err) throw err;
+        console.log("Game created in DB");
+        db.close();
+        startGame(dbResponse._id);
+      });
+    });
+  
+}
+
+function startGame(gameId)
+{
+    console.log('starting game');
+    console.log(gameId);
+    //tell all clients to start the game and send their respective player id 
+    for(var i=0; i<connections.length;i++)
+    {
+      var playerId = i + 1;
+      io.to(connections[i]).emit('gameStart',{gameId:gameId,playerId:playerId,dbResponse:dbResponse});
+    }
+}
+
+
+
+function getInitialDb()
+{
+  var db = {
+  "status": "InProgess",
+  "numberOfPlayers": 2,
+  "currentNumberOfPlayers": 2,
+  "nextTurn": "1",
+  "players": [{
+      "id": 1,
+      "balance": 1500,
+      "networth": 1500,
+      "status": "playing",
+      "currentPositionInBoard": 1
+    },
+    {
+      "id": 2,
+      "balance": 1500,
+      "networth": 1500,
+      "status": "playing",
+      "currentPositionInBoard": 1
+    }
+  ],
+  "properties": [{
+    "id": 1,
+    "name": "winterfell",
+    "owner": "noone",
+    "color": "blue",
+    "rent": [10, 100, 200, 300],
+    "currentState": 0
+  }, {
+    "id": 2,
+    "name": "dragonstone",
+    "owner": "noone",
+    "color": "red",
+    "rent": [5, 50, 150, 200],
+    "currentState": 0
+  }]
+  }
+  return db;
 }
--- js/game.js
+++ js/game.js
@@ -1,19 +1,38 @@
 var socket = io.connect('http://localhost:8080');
 var app = angular.module("westeros", []); 
+var dbResponse;
+
 app.controller("myCtrl", function($scope) {
-    $scope.quantity = 1;
-    
-    socket.on('gameStart',function(data){
-        console.log('player id: '+data.playerId);
-        sessionStorage.playerId = data.playerId;
+      $scope.message = "";
+      $scope.balance = 0;
+      $scope.networth = 0;
+      
+      $scope.rollDice = function(){
+         console.log('rolling the dice');
+         socket.emit('rollDice', { playerId: sessionStorage.playerId }); 
+    }
+    //Game cannot start. Have to wait until server tells me to start.
+    //disable roll button
+    socket.on('wait',function(data){
+        console.log('waiting for more players to join');
+        $scope.message = "waiting for more players to join";
+        $scope.disableButton = true;
+        $scope.$apply();
     });
 
-
-    $scope.rollDice = function(){
-       console.log('player turn');
-       socket.emit('rollDice', { playerId: sessionStorage.playerId }); 
-    }
-
+    socket.on('gameStart',function(data){
+        console.log('Starting game. My player id is '+data.playerId);
+        sessionStorage.playerId = data.playerId;
+        sessionStorage.gameId = data.gameId;
+        dbResponse = data.dbResponse;
+        $scope.balance = dbResponse.players[Number(sessionStorage.playerId)-1].balance;
+        $scope.networth = dbResponse.players[Number(sessionStorage.playerId)-1].networth;
+        $scope.disableButton = toggleRollButton();
+        if($scope.disableButton)
+          rotateBoard(document.getElementById('gameboard'),180);
+        $scope.message = "Game Started! "+getNextTurn($scope.disableButton)+" turn";        
+        $scope.$apply();
+    });
     
     socket.on('move',function(data){
       console.log('move player'+data.playerId+' : ' + data.diceNumber);
@@ -23,73 +42,3 @@ app.controller("myCtrl", function($scope) {
 
 });
 
-function movePlayerInUi(playerId,diceNumber)
-{   
-    document.getElementById(diceNumber).appendChild( document.getElementById('player'+playerId));
-}
-
-function animateMovement(playerId,diceNumber) {
-  var elem = document.getElementById("player1"); 
-  elem.classList.add("animation_down");
-  var pos = 0;
-  var id = setInterval(frame, 5);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-      elem.classList.remove("animation_down");
-      elem.style.top = '0px'; 
-      elem.style.left = '0px'; 
-      movePlayerInUi(playerId,diceNumber)
-    } else {
-      pos++; 
-      elem.style.top = pos + 'px'; 
-      elem.style.left = pos + 'px'; 
-    }
-  }
-}
-
-
-/*
-function createInnerDivs()
-{
-    var i,j;
-    var iDiv = document.createElement('div');
-    iDiv.className = 'iDiv';
-    var N = 30;
-    var j = 1;
-    for(var i=1;i<=N;i++)
-  {
-                
-        var iDiv = document.createElement('div');
-        if(i==1 || i==9 || i==22 || i==30)
-        {
-            iDiv.className = "corner";
-            iDiv.id = j++;
-        }
-        else if(i==11 || i==14 || i==17 || i==20)
-        {
-            iDiv.className="middle";
-        }
-        else if(i==10 || i==12 || i==13 || i==15 || i==16 || i==18 || i==19 || i==21)
-        {
-            iDiv.className="vertical";
-            iDiv.id = j++;
-        }
-        else
-        {
-            iDiv.className="horizontal";
-            iDiv.id = j++;
-        }
-        
-        iDiv.innerHTML = iDiv.id;
-        
-        console.log(" a "+iDiv.id);
-       
-        document.getElementById('gameboard').appendChild(iDiv);
-    
-    }
-    //document.write('<br/>');
-  
-}
-
-*/
